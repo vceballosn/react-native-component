d199da813d94ea5c33a300fe4101edb2b66a0f12
vceballosn <vladimir.ceballos@gmail.com>
2025-08-19 20:57:23 -0400

    component </TouchableHighlight>

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image,Button,TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, Image,Button,TouchableHighlight,Pressable } from 'react-native';
 
 const icon = require("./assets/icon.png")
 
@@ -13,13 +13,23 @@ export default function App() {
        />
       <Text style={{color:'white' }}>Vladimir Ceballos Exitoso!</Text>
       <Button title='pulse here 'onPress={()=> alert('hello Vladimir Ceballos') } color="red"/>
-      <TouchableHighlight
-        style={styles.button}
-        underlayColor="#09f"
-        onPress={() => alert('¡Hola, Vladimir Ceballos!')}
+     <TouchableHighlight
+  style={styles.redRoundedButton}
+  underlayColor="#cc0033" // Un rojo más oscuro para el efecto al presionar
+  onPress={() => alert('¡Hola, Vladimir Ceballos!')}
+>
+  <Text style={styles.buttonText}>Presiona Aquí</Text>
+</TouchableHighlight>
+<Pressable
+        style={({ pressed }) => [
+          styles.redRoundedButton,
+          pressed ? styles.buttonPressed : styles.basicButton,
+        ]}
+        onPress={() => alert('¡Presionaste el botón!')}
       >
-        <Text style={styles.buttonText}>Presiona Aquí</Text>
-      </TouchableHighlight>
+        <Text style={styles.text}>Presiona Aquí2222</Text>
+      </Pressable>
+
     </View>
   );
 }
@@ -35,5 +45,14 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+   basicButton: {
+    backgroundColor: '#007AFF',
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 8,
+    alignItems: 'center',
+  },buttonPressed: {
+    backgroundColor: '#005bb5', // Color cuando se presiona
   },
 });
